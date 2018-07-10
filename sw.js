d24b9cee5cfa79d05c43c7374e7151f07f07e0b8
pokerica <34429406+pokerica@users.noreply.github.com>
2018-07-10 21:30:48 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// *** abcdefgh
+self.addEventListener('install', e => {
+  caches.delete('pmpAppCache').then(cache => {
+      caches.open('pmpAppCache').then(cache => {
+        return cache.addAll(
+        [ 
+          '/', '/index.html', '/manifest.json',
+          '/client.js', '/icons/aux.js', '/style.css',
+          '/icons/ibm.ttf', '/icons/quack.wav' 
+        ]).then(() => self.skipWaiting());
+      });
+  });
+});
+
+self.addEventListener('activate', event => {self.clients.claim(); });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true})
+      .then( response => {return response || fetch(event.request)} )
+  );
+});
