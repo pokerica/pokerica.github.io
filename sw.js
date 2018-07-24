ff9e4785a801af7bd981561703292bf4c3e9b0b9
Glitch (p23) <none>
2018-07-24 19:57:27 +0000

    23a

---
+++ sw.js
@@ -0,0 +1,15 @@
+
+// *** r23 a
+self.addEventListener('install', e => {
+  caches.delete('pmpAppCache').then(cache => {
+      caches.open('pmpAppCache').then(cache => {
+        return cache.addAll([ '/', '/index.html', '/client.js', '/icons/aux.js',
+          '/style.css', '/manifest.json', '/icons/ibm.ttf', '/icons/quack.wav' ])
+          .then(() => self.skipWaiting());
+      }); });
+});
+self.addEventListener('activate', event => {self.clients.claim(); });
+self.addEventListener('fetch', event => {event.respondWith(
+  caches.match(event.request, {ignoreSearch:true})
+    .then( response => {return response || fetch(event.request)} ) );
+});
