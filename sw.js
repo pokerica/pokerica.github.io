1bfacdb41bc96a93adb03e9d9bd89f5aecdd83ac
pokerica <34429406+pokerica@users.noreply.github.com>
2018-07-24 22:17:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 self.addEventListener('install', e => {
   caches.delete('pmpAppCache').then(cache => {
       caches.open('pmpAppCache').then(cache => {
-        return cache.addAll([ '/', '/index.html', '/client.js', '/icons/aux.js',
-          '/style.css', '/manifest.json', '/icons/ibm.ttf', '/icons/quack.wav' ])
+        return cache.addAll([ '/', '/index.html', '/client.js', '/aux/aux.js',
+          '/style.css', '/manifest.json', '/aux/ibm.ttf', '/aux/quack.wav' ])
           .then(() => self.skipWaiting());
       }); });
 });
