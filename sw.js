9664204e91dc8fb6e2b1094a1fcac2205f01e371
Glitch (p25) <none>
2018-09-22 18:59:42 +0000

    25f

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-// *** 23d 24m 25a
-// *** 25 abcde
-var vrz= 'p25e';
+// *** 23d 25e
+var vrz= 'p25f';
+// *** 25abcdef 012345789
 self.addEventListener('install', function(event)
 {
   event.waitUntil(
     caches.open(vrz).then(function(cache) {
-        return cache.addAll([ '/', 'index.html', 'client.js',
-          'aux/aux.js', 'style.css', 'manifest.json',
-          'aux/icon-144.png', 'aux/ibm.ttf', 'aux/quack.wav' ]);
+        return cache.addAll([ '/',          
+          'index.html', 'client.js',
+          'aux/aux.js', 'style.css',
+          'manifest.json', 'aux/icon-144.png',
+          'aux/ibm.ttf', 'aux/quack.wav' ]);
     }).then(function() {
       return self.skipWaiting();
     })
